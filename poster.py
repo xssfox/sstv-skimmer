18c85f83054e6532c3f4307fd48aef187a72c3f1
xss <michaela@michaela.lgbt>
2023-08-05 10:01:07 +1000

    fix frequency tags

--- poster.py
+++ poster.py
@@ -30,46 +30,12 @@ print("Logged into Mastodon")
 patterns = ["*.png"]
 my_event_handler = FileSystemEventHandler()
 
-def caption(image_path):
-    url = "https://artemai.art/api/caption/"
-
-    payload = json.dumps({
-        "image": base64.encodebytes(open(image_path,"rb").read()).decode("utf-8")
-    })
-    headers = {
-    'Content-Type': 'application/json'
-    }
-    print("Generated payload for Artemai request")
-
-    response = requests.request("POST", url, headers=headers, data=payload).json()
-    print(f"Got response - {response}")
-    caption_text = response['caption']
-    ocr = response["ocr"]
-    greyPercent = response["greyPercent"]
-    psnr = response["psnr"]
-    denoised = response["denoised"]
-
-    return caption_text, ocr, greyPercent, psnr, denoised
 
 def on_created(event):
     try:
         # create post
         print(f"new Image: {event.src_path}")
-        try:
-            caption_text, ocr, greyPercent, psnr, denoised  = caption(event.src_path)
-            alt_text = f"SSTV Auto caption: {caption_text}"
-            if ocr != "":
-                alt_text += f". Text in image: {ocr}"
-            extra_text = f" G:{greyPercent} P:{psnr}"
-            print(f"worked out text {alt_text} {extra_text}")
-            denoised_media = mastodon.media_post(base64.decodebytes(denoised.encode("utf-8")), "image/png", description=alt_text)
-            print(f"Uploaded denoised media")
 
-        except:
-            print("issue with Artemai request")
-            print(traceback.format_exc())
-            alt_text = ""
-            extra_text = ""
         time.sleep(2)
         if event.src_path.endswith(".jp2"):
             # treat as DRM
@@ -77,27 +43,23 @@ def on_created(event):
             cv2.imwrite('/tmp/drm.png', image)
             path = '/tmp/drm.png'
             sstv_mode = "DRM"
-            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']}. Filename: {event.src_path.split('/')[-1]}\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}\n{extra_text}"
+            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']}. Filename: {event.src_path.split('/')[-1]}\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}kHz\n"
         elif event.src_path.startswith("/drm/"):
             path = event.src_path
             sstv_mode = "DRM"
-            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']}. Filename: {event.src_path.split('/')[-1]}\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}\n{extra_text}"
+            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']}. Filename: {event.src_path.split('/')[-1]}\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}kHz\n"
         else:
             path = event.src_path
             sstv_mode, date, time_var = event.src_path.split("/")[-1].split("_")
             date = event.src_path.split("/")[-1].split("_")[1]
             date = f"{date[:4]}-{date[4:6]}-{date[6:]}"
             time_var = f"{time_var[:2]}:{time_var[2:4]}:{time_var[4:6]}"
-            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']} at {date} {time_var} UTC\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}\n{extra_text}"
+            message = f"SSTV {sstv_mode} Image received on {int(os.environ['FREQ'])/1000000:.3f} MHz {os.environ['MODE']} at {date} {time_var} UTC\n#sstv #{sstv_mode} #{int(os.environ['FREQ'])/1000:.0f}kHz\n"
         
-        media = mastodon.media_post(path, "image/png", description=alt_text)
+        media = mastodon.media_post(path, "image/png", description="Image received by slow scan television")
         
         media_ids = [media["id"]]
-        try:
-            media_ids.append(denoised_media["id"])
-            print("added denoised media to post")
-        except:
-            pass
+
         print(message)
         if sstv_mode != "BW12":
             mastodon.status_post(message, media_ids=media_ids, visibility="unlisted")
